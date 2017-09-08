573522af65d16f5276856056713717c2ee876dfe
Will Szumski <will@cowboycoders.org>
2017-09-08 21:27:53 +0100

    non standard byte intepretations in separate file

---
+++ src/main/java/org/fluxoid/utils/bytes/NonStandardOps.java
@@ -0,0 +1,17 @@
+package org.fluxoid.utils.bytes;
+
+
+/**
+ * Bizarre, but true? or maybe a bug...time will tell.
+ */
+class LESpecialOps {
+
+
+    public short get_F0FF(byte[] data, int val){
+        return 0;
+    }
+
+    public void put_F0FF(byte[] data, int offset, int val) {
+
+    }
+}
